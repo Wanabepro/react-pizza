0532e0e19f8e81bfb8b4f2f86633a2543648c1a4
Sergei Iliasov <ilyasov_sk@mail.ru>
2023-03-29 15:17:03 +0300

    router switched to hash router

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import { App } from "./App";
@@ -8,8 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
     <Provider store={store}>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter basename={process.env.PUBLIC_URL}>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 );
